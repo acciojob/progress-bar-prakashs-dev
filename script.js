3b7f5e36f4476679b0b6af62f762a4723618bb87
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-12-01 14:52:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+let currentElement = document.querySelector(".circle:first-child");
+      const prevButton = document.getElementById("prev");
+      const nextButton = document.getElementById("next");
+      let position = 1;
+      function next() {
+        if (prevButton.disabled) prevButton.disabled = false;
+        if (position < 5) {
+          const nxtLine = currentElement.nextElementSibling;
+          nxtLine.classList.add("active");
+          const nxtCircle = nxtLine.nextElementSibling;
+          nxtCircle.classList.add("active");
+
+          currentElement = nxtCircle;
+          position++;
+          if (position === 5) {
+            nextButton.disabled = true;
+          }
+        } 
+      }
+      function prev() {
+        if (nextButton.disabled) nextButton.disabled = false;
+        if (position !== 1) {
+          currentElement.classList.remove("active");
+          const line = currentElement.previousElementSibling;
+          line.classList.remove("active");
+
+          currentElement = line.previousElementSibling;
+          position--;
+          if (position === 1) {
+            prevButton.disabled = true;
+          }
+        }
+      }
